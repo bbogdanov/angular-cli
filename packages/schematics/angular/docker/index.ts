abe88dd64d668394e6bc44a886df000e1a4ed36b
Bogdan Bogdanov <bogdanmbogdanov@gmail.com>
2018-08-17 16:03:23 +0300

    add to angular.json project environment a docker configration

--- packages/schematics/angular/docker/index.ts
+++ packages/schematics/angular/docker/index.ts
@@ -8,23 +8,70 @@
 import { strings } from '@angular-devkit/core';
 import {
   Rule,
+  SchematicContext,
   SchematicsException,
   Tree,
   apply,
+  chain,
   mergeWith,
   move,
   template,
   url,
 } from '@angular-devkit/schematics';
 import { dirname } from 'path';
-import { getWorkspace } from '../utility/config';
+import { getWorkspace, getWorkspacePath } from '../utility/config';
+import { getProjectTargets } from '../utility/project-targets';
 import { Path } from './../../../angular_devkit/core/src/virtual-fs/path';
 import { Schema as DockerOptions } from './schema';
 
+function updateConfigFile(options: DockerOptions): Rule {
+  return (host: Tree, context: SchematicContext) => {
+
+    const dockerOptions = {
+      imageName: options.imageName,
+      registryAddress: options.imageRegistry,
+      machineName: options.machineName,
+      isImageDeploy: false,
+      serviceName: options.serviceName,
+    };
+
+    context.logger.debug('updating config file.');
+    const workspacePath = getWorkspacePath(host);
+
+    const workspace = getWorkspace(host);
+
+    const project = workspace.projects[options.project as string];
+
+    if (!project) {
+      throw new Error(`Project is not defined in this workspace.`);
+    }
+
+    const projectTargets = getProjectTargets(project);
+
+    if (!projectTargets) {
+      throw new Error(`Target is not defined for this project.`);
+    }
+
+    let applyTo = projectTargets.build;
+
+    if (options.environment &&
+        projectTargets.build.configurations &&
+        projectTargets.build.configurations[options.environment]) {
+      applyTo = projectTargets.build.configurations[options.environment];
+    }
+
+    applyTo.docker = dockerOptions;
+
+    host.overwrite(workspacePath, JSON.stringify(workspace, null, 2));
+
+    return host;
+  };
+}
 
 export default function (options: DockerOptions): Rule {
   return (host: Tree) => {
     const workspace = getWorkspace(host);
+
     if (!options.project) {
       throw new SchematicsException('Option (project) is required.');
     }
@@ -45,6 +92,9 @@ export default function (options: DockerOptions): Rule {
       move(options.path),
     ]);
 
-    return mergeWith(templateSource);
+    return chain([
+      mergeWith(templateSource),
+      updateConfigFile(options),
+    ]);
   };
 }
